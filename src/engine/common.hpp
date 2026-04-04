336c41167350a951d4e0a66d33135b49f6af9862
UNSTOP4BLE <88965856+UNSTOP4BLE@users.noreply.github.com>
2026-04-05 00:49:21 +0200

    clean shit up

--- src/engine/common.hpp
+++ src/engine/common.hpp
@@ -5,15 +5,6 @@
 #include <stdint.h>
 
 namespace ENGINE::COMMON {
-
-    constexpr int SCREEN_WIDTH  = 320;
-    constexpr int SCREEN_HEIGHT = 240;
-
-    //only useful on ps1
-    constexpr int DMA_MAX_CHUNK_SIZE  = 16;
-    constexpr int CHAIN_BUFFER_SIZE   = 4104;
-    constexpr int ORDERING_TABLE_SIZE = 1024;
-
     //common definitions
     using RECT32   = ENGINE::TEMPLATES::RECT<int32_t>;
     using XY32     = ENGINE::TEMPLATES::XY<int32_t>;
